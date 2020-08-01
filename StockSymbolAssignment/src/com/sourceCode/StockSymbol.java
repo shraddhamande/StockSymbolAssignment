f354f56a54a6777816247e413cbfb941fb045726
Tushar Mande <Tushar Mande@DESKTOP-S55P5L2>
2020-08-01 22:31:21 +0530

    Source code for StockSymbol.java

--- StockSymbolAssignment/src/com/sourceCode/StockSymbol.java
+++ StockSymbolAssignment/src/com/sourceCode/StockSymbol.java
@@ -1,5 +1,59 @@
 package com.sourceCode;
+import java.util.*;
+public class StockSymbol
+{
+	String symbol,timeStamp;
+	float high,low,range;
+	public String getSymbol() {
+		return symbol;
+	}
+	public void setSymbol(String symbol) {
+		this.symbol = symbol;
+	}
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public float getHigh() {
+		return high;
+	}
+	public void setHigh(float high) {
+		this.high = high;
+	}
+	public float getLow() {
+		return low;
+	}
+	public void setLow(float low) {
+		this.low = low;
+	}
+	public float getRange() {
+		return range;
+	}
+	public void setRange(float range) {
+		this.range = range;
+	}
 
-public class StockSymbol {
+	public static Comparator<StockSymbol> TimeStampComparator = new Comparator<StockSymbol>() 
+	{
 
+		public int compare(StockSymbol s1, StockSymbol s2)
+		{
+		   String timeStamp1 = s1.getTimeStamp().toUpperCase();
+		   String timeStamp2 = s2.getTimeStamp().toUpperCase();
+		   
+		   return timeStamp1.compareTo(timeStamp2);//ascending
+
+		   //return timeStamp2.compareTo(timeStamp1);//Descending
+	    }
+	};
+
+	
+	@Override
+	public String toString() {
+		return " [symbol = " + symbol + ", range = " + range + ", timeStamp = " + timeStamp +"]\n";
+	}
+	
 }
+
